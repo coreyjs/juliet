ff8179257eefb51622188485c2cb28a4b8ef6e3e
Corey Schaf <cschaf@gmail.com>
2017-01-30 17:00:11 -0500

    podspec added

---
+++ Juliet/Juliet.h
@@ -0,0 +1,19 @@
+//
+//  Juliet.h
+//  Juliet
+//
+//  Created by Corey Schaf on 1/30/17.
+//  Copyright © 2017 Rogue Bit Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Juliet.
+FOUNDATION_EXPORT double JulietVersionNumber;
+
+//! Project version string for Juliet.
+FOUNDATION_EXPORT const unsigned char JulietVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Juliet/PublicHeader.h>
+
+
